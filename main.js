2f9e7c407890572d0f1e8563eb08320e33dc6671
najmiddino <164999098+najmiddino@users.noreply.github.com>
2024-05-30 15:04:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+alert  ("sizda random sonlar chiqishni boshlaydi");
+
+let random_num = Math.floor(Math.random() * 100)
+
+alert  (`
+random orqali chiqqan son:${random_num}
+`);
+
+confirm ('biz ularni 2ga kopaytirib 2ga bolamiz')
+
+
+alert (`
+random orqali chiqqan son:${random_num}
+biz uni 2ga kopaytirdik ${random_num * 2} 
+biz uni 2ga boldik ${random_num / 2} 
+usha sonni 3ga bolganda qoldiq:${random_num % 3}  
+`)
+
+console.log = ('biz ularni 2ga kopaytirib 2ga bolamiz')
+
+
